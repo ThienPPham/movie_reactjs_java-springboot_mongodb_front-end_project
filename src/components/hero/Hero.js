2bbb3a84255bc6a95eecaea0fc3e0160b7846a00
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-10-08 11:28:24 +0700

    create home and hero component

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '/.Hero.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+
+const Hero = ({movies}) => {
+  return (
+    <div>
+      <Carousel>
+        
+      </Carousel>
+    </div>
+  )
+}
+
+export default Hero
