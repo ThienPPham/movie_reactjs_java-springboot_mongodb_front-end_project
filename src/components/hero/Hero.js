4b69ed1236d50b7d30aa7b69086925c2adb41460
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-10-10 15:32:24 +0700

    style the carousel

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import '/.Hero.css';
+import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
-import { Paper } from '@mui/material';
+import { Backdrop, Paper } from '@mui/material';
 
 const Hero = ({movies}) => {
   return (
     <div>
       <Carousel>
-        
+        {
+            movies?.map((movie) => {
+                return(
+                    <Paper>
+                        <div className='movie-card-container'>
+                            <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                <div className='movie-detail'>
+                                    <div className='movie-poster'>
+                                        <img src={movie.poster} alt=''/>
+                                    </div>
+                                    <div className='movie-title'>
+                                        <h4>{movie.title}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Paper>
+                )
+            })
+        }
       </Carousel>
     </div>
   )
